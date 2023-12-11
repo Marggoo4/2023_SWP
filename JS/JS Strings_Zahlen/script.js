5939510cee4ce57e446d00248bf448f4ee495649
Marco <marco.bischof@student.htldornbirn.at>
2023-12-11 12:14:18 +0100

    JS_Strings_Zahlen_und_Reload

---
+++ JS/JS Strings_Zahlen/script.js
@@ -0,0 +1,9 @@
+let a = 17.4;
+let b = 3.9;
+
+console.log("The result of", a, "divided by", b, "is", a / b);
+
+let firstname = "MARCO";
+let lastname = " BISCHOF";
+
+console.log("Ich heiße", firstname + lastname);
