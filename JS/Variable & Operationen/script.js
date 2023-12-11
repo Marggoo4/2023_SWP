615ed986b4b21d31a324afb20258ee4a72279b02
Marco <marco.bischof@student.htldornbirn.at>
2023-12-11 11:51:44 +0100

    Übung Variable & Operationen

---
+++ JS/Variable & Operationen/script.js
@@ -0,0 +1,27 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+let a = 7;
+let b = 8;
+let c = 10;
+
+//console 
+
+let result = a + b + c;
+console.log("Ergebnis ist" + result);
+console.log("Ergebnis ist", (a + b) / c);
+console.log("Ergebnis ist", c - a + b);
+console.log("Ergebnis ist", a + b + c * 3);
+console.log("Ergebnis ist", (a + b + c) / 4);
+
+// Variable a auf 3 geändert
+
+a = 3;
+b = a + 3;
+c = c -3
+
+console.log("Ergebnis ist", a + b + c);
+console.log("Ergebnis ist", (a + b) / c);
+console.log("Ergebnis ist", c - a + b);
+console.log("Ergebnis ist", a + b + c * 3);
+console.log("Ergebnis ist", (a + b + c) / 4);
