82f2f136f5b06312fe83f4c33f56df5e91688cb1
Marco <marco.bischof@student.htldornbirn.at>
2023-12-18 11:11:00 +0100

    Aufgabe_03 finished

--- JS/Aufgabe_03/script.js
+++ JS/Aufgabe_03/script.js
@@ -4,4 +4,26 @@ let apple = "Apple";
 let bananaPricePerKilo = 2.24;
 let applePricePerKilo = 3.43;
 
-let
+let Apfel = 0.34;
+// Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo 
+
+let Banane = 0.22;
+// Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo
+
+console.log("Preis von 8 Äpfeln", 8 * Apfel *  applePricePerKilo);
+// Preis von 8 Äpfeln 
+
+
+console.log("Preis von 17 Bananen", 17 * Banane * bananaPricePerKilo);
+// Preis von 17 Bananen 
+
+console.log("Preis von 1 Tonne Äpfel", applePricePerKilo * 1000);
+// Preis von 1 Tonne Äpfel 
+
+console.log("Preis von 1 Tonne Bananen", bananaPricePerKilo * 1000)
+// Preis von 1 Tonne Bananen 
+
+
+
+
+
