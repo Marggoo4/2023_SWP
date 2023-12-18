19531d1e7297eb8e760f91ec8f38f252f6e46768
Marco <marco.bischof@student.htldornbirn.at>
2023-12-18 11:51:25 +0100

    Aufgabe_04 finished

---
+++ JS/Aufgabo_04/script.js
@@ -0,0 +1,43 @@
+console.log("test");
+
+// Zähle von 7 bis 45 
+for (let count = 7; count<45; count++) {
+    console.log("Marco")
+}
+
+// Zähle von 9 bis 99 
+for (let count = 9; count<99; count++) {
+    console.log("Marco")
+}
+
+
+
+// Zähle rückwärts von 10 bis 0 
+for (let count = 10; count>=0; count--) {
+    console.log(count)
+}
+
+// Zähle rückwärts von 1234 bis 1207
+for (let count=1234; count>=1207; count--) {
+    console.log(count)
+}
+
+ // Gib Deinen Namen genau 8 mal aus
+ for (let count = 1; count<=8; count++) {
+    console.log(count + " Marco")
+ }
+
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, 
+for (let count = 0; count<=10; count++) {
+    console.log(count)
+}
+
+let myName = "Marco"
+console.log("Mein Name ist", myName)
+
+//dann zähle  von 10 auf 0
+for (let count = 10; count>=0; count--) {
+    console.log(count)
+}
+
+console.log("Mein Name ist", myName);
